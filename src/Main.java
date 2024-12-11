e602d88f080607a2bff983a83d8bdce565fe063d
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-10 22:18:45 -0600

    ArrayList commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.Callable;
+
+public class Main {
+    public static void main(String[] args) {
+        CreateArrayListExample createArrayListExample = new CreateArrayListExample();
+        createArrayListExample.ArrayListDemo();
+        createArrayListExample.CreateArrayListFromCollectionExample();
+        createArrayListExample.CreateArrayListFromCollectionExample2();
+        createArrayListExample.RemoveElementFromTheArrayList();
+        createArrayListExample.IterateOverArrayList();
+        createArrayListExample.RemoveElementFromTheArrayList();
+    }
+
+}
